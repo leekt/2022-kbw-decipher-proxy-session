fc4b7e3d424875f60501808e9a8b5165cceccb20
leekt <leekt216@gmail.com>
2022-08-10 23:23:07 +0900

    initial layout

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+contract Contract {
+    address public owner;
+    uint256 public count;
+    uint256 public version;
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
